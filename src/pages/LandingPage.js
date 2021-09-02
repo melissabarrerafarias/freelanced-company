ecf92533d44098d1415c32941a19ba0cc4964589
melissikiss <melissikiss@gmail.com>
2021-09-02 16:00:43 -0700

    added carousel, needs styling

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,35 @@
-import React from 'react'; 
+import React from 'react';
+import '../landing-page.css';
+import { Carousel } from 'react-bootstrap'
+
+import placeholder1 from '../assets/images/placeholder-1.jpg';
+import placeholder2 from '../assets/images/placeholder-2.jpg';
+import placeholder3 from '../assets/images/placeholder-3.jpg';
 
 function LandingPage() {
     return (
-        <main> 
-            
-        </main>
+        <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="5000">
+                    <img src={placeholder1} class="d-block w-100" alt="..."></img>
+                </div>
+                <div class="carousel-item" data-bs-interval="5000">
+                    <img src={placeholder2} class="d-block w-100" alt="..."></img>
+                </div>
+                <div class="carousel-item" data-bs-interval="5000">
+                    <img src={placeholder3} class="d-block w-100" alt="..."></img>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     )
 }
 
-export default LandingPage
+export default LandingPage;
