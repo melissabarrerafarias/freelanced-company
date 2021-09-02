de3eb6ab90e4c2d882498cf288f895084563e139
melissikiss <melissikiss@gmail.com>
2021-09-02 14:09:07 -0700

    deleted code, pushing to develop branch for clean slate to pull from

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -17,24 +17,9 @@ function NavBar() {
                             <a class="nav-link" aria-current="page" href="#">Contact</a>
                         </li>
                     </ul>
-                </div>
+                </div> 
             </div>
         </nav>
-        // <nav className="navbar navbar-expand-lg navbar-light navbar-background">
-        //     <div className="container-fluid">
-        //         <a className="navbar-brand" href="#">Spacebarr</a>
-        //     </div>
-        //     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        //         <span class="navbar-toggler-icon"></span>
-        //     </button>
-        //     <div class="collapse navbar-collapse" id="navbarNav">
-        //         <ul class="navbar-nav">
-        //             <li class="nav-item">
-        //                 <a class="nav-link" aria-current="page" href="/contact">Contact</a>
-        //             </li>
-        //         </ul>
-        //     </div>
-        // </nav >
     )
 }
 
