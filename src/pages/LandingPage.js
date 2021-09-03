45b96210d8694e09482570e161c177924efbdb28
melissikiss <melissikiss@gmail.com>
2021-09-03 11:10:25 -0700

    finished basic layout for offer and work section/images need work

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -32,6 +32,7 @@ function LandingPage() {
                     </button>
                 </div>
             </div>
+
             <div className="container-fluid">
                 <h1 className="offer-heading text-center">What We Offer</h1>
 
@@ -44,12 +45,56 @@ function LandingPage() {
                     </div>
 
                     <div className="col-md-6">
-                        <i class="fas fa-tools offer-icons"></i>
+                        <i className="fas fa-tools offer-icons"></i>
                         <p className="offer-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
                             nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
                             eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
                     </div>
                 </div>
+            </div>
+
+            <h1 className="offer-heading text-center">Our Work</h1>
+            <div className="container-fluid d-flex justify-content-center">
+
+                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src={placeholder1} class="d-block w-100 example-images" alt="..."></img>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src={placeholder2} class="d-block w-100 example-images" alt="..."></img>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the third slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src={placeholder3} class="d-block w-100 example-images" alt="..."></img>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the third slide.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+
             </div>
         </main>
     )
