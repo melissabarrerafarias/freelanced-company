21d0fd70dae2fa84f412c669cb5eded60cc476fa
melissikiss <melissikiss@gmail.com>
2021-09-08 09:10:40 -0700

    mobile responsive contact page info

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,35 +10,34 @@ function Contact() {
                 <img src={computerImg} className="contact-img"></img>
             </div>
 
-            <div className="row">
-
-                <div className="col-md-6 text-center d-flex justify-content-md-end">
-                    <div className="business-hours-div">
-                        <h3 className="business-heading">Business Hours</h3>
-                        <ul className="hours">
-                            <li>Mondays 9:00am - 5:00pm</li>
-                            <li>Tuesdays 9:00am - 5:00pm</li>
-                            <li>Wednesdays 9:00am - 5:00pm</li>
-                            <li>Thursdays 9:00am - 5:00pm</li>
-                            <li>Fridays 9:00am - 5:00pm</li>
-                        </ul>
+            <div className="container"> {/* row must always be placed inside container (will cause white space if not) */}
+                <div className="row">
+                    <div id = "businessHours" className="col-md-6 text-center d-flex justify-content-md-end d-flex justify-content-sm-center">
+                        <div className="business-hours-div">
+                            <h3 className="business-heading">Business Hours</h3>
+                            <ul className="hours">
+                                <li>Mondays 9:00am - 5:00pm</li>
+                                <li>Tuesdays 9:00am - 5:00pm</li>
+                                <li>Wednesdays 9:00am - 5:00pm</li>
+                                <li>Thursdays 9:00am - 5:00pm</li>
+                                <li>Fridays 9:00am - 5:00pm</li>
+                            </ul>
+                        </div>
                     </div>
 
-                </div>
+                    <div id="contact-info" className="col-md-6 text-center">
+                        <div className="contact-divs">
+                            <i class="fas fa-phone-alt contact-logos"></i>Call Us
+                            <a href="tel:408-642-8028"> at 408-642-8028</a>
+                        </div>
 
-                <div id="contact-info" className="col-md-6 text-center">
-                    <div className="contact-divs">
-                        <i class="fas fa-phone-alt contact-logos"></i>Call Us
-                        <a href="tel:408-642-8028"> at 408-642-8028</a>
-                    </div>
-
-                    <div className="contact-divs">
-                        <i class="fas fa-envelope-square contact-logos"></i>Or Email Us at
-                        <a href="mailto:spacebarr@gmail.com"> @spacebarr@gmail.com</a>
+                        <div className="contact-divs">
+                            <i class="fas fa-envelope-square contact-logos"></i>Or Email Us at
+                            <a href="mailto:spacebarr@gmail.com"> @spacebarr@gmail.com</a>
+                        </div>
                     </div>
                 </div>
             </div>
-
         </main>
     )
 }
