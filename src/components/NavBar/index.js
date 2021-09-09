5b6c614561e7fb296b0ebc31a17a0af511d04d44
melissikiss <melissikiss@gmail.com>
2021-09-09 14:01:38 -0700

    added brand name variaton

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,9 +4,10 @@ import '../../navbar.css';
 function NavBar() {
     return (
         <nav class="navbar navbar-expand-lg navbar-light navbar-background">
-            <div id = "navBarElements" class="container-fluid">
-                <a id = "navTitle" class="navbar-brand" href="/">SPACEBARR</a>
-
+            <div id="navBarElements" class="container-fluid">
+                <div id="navTitle">
+                    <a class="navbar-brand" href="/">SPACE<span className="barr">BARR</span></a>
+                </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fas fa-chevron-circle-down"></i>
                 </button>
@@ -14,13 +15,13 @@ function NavBar() {
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a id = "navFAQ" class="nav-link" aria-current="page" href="/#FAQ">FAQ</a>
+                            <a id="navFAQ" class="nav-link" aria-current="page" href="/#FAQ">FAQ</a>
                         </li>
                         <li class="nav-item">
-                            <a id = "navAbout" class="nav-link" aria-current="page" href="/#about">ABOUT US</a>
+                            <a id="navAbout" class="nav-link" aria-current="page" href="/#about">ABOUT US</a>
                         </li>
                         <li class="nav-item">
-                            <a id = "navContact" class="nav-link" aria-current="page" href="/contact">CONTACT</a>
+                            <a id="navContact" class="nav-link" aria-current="page" href="/contact">CONTACT</a>
                         </li>
                     </ul>
                 </div>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -11,7 +11,7 @@ function LandingPage() {
         <main>
             {/* front page carousel */}
             <div id="#mainCarousel">
-                <div class="carousel-branding"><h1 className="brand-name">SPACEBARR</h1></div>
+                <div class="carousel-branding"><h1 className="brand-name">SPACE<span className = "barr">BARR</span></h1></div>
                 <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active" data-bs-interval="5000">
